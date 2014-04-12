cb4576113d0d4baa271367c9005109c525e9b4c8
ArtificerZeno <picka@optonline.net>
2014-04-11 23:31:01 -0500

    made an initializer for pather

--- toufuu_source/Assets/Scripts/Logic/Models/MiniBoss.cs
+++ toufuu_source/Assets/Scripts/Logic/Models/MiniBoss.cs
@@ -1,36 +1,67 @@
 using UnityEngine;
 using System.Collections;
 
-public class MiniBoss : MonoBehavior {
+public class MiniBoss : MonoBehaviour {
 	//this is the class for a generic mini boss and all of its functions/vars
 	
 	#region Variables
 	//health of the mini
-	public int health;
+	public float health;
 	
-	//the three attack timers and start times
-	//attackx_t is the current timer till next attack
-	//attackx_s is the start of the timer after attack
-	public int attack1_t; 
-	public int attack2_t;
-	public int attack3_t;
-	public int attack1_s;
-	public int attack2_s;
-	public int attack3_s;
+	//the three attack timers and their bools
+	//attackxL is the time at last use of the ability
+	//attackxR is the bool for being ready
+	public Time attack1L; 
+	public Time attack2L;
+	public Time attack3L;
+	public bool attack1R;
+	public bool attack2R;
+	public bool attack3R;
 	
 	//the movement path of the mini
-	public Path mypath;
+	public Pather mypath;
 	#endregion
 	
 	#region Functions
+	
+	//void functions to execute the attacks to be overwriten by children
+	#region Attacks
+	public void attack1(){}
+	public void attack2(){}
+	public void attack3(){}
+	#endregion
+	
+	//check cooldowns
+	public void coolCheck() {
+		if (Time.timeSinceLevelLoad - attack1L <=0) {attack1R = true;}
+		if (Time.timeSinceLevelLoad - attack2L <=0) {attack2R = true;}
+		if (Time.timeSinceLevelLoad - attack3L <=0) {attack3R = true;}
+	}
+	
 	// Use this for initialization
 	void Start () {
-	
+		//prepair all the attacks
+		attack1R = true;
+		attack2R = attack3R = false;
+		//set distonance between attacks
+		attack2L = Time.timeSinceLevelLoad+3.0;
+		attack3L = Time.timeSinceLevelLoad+7.0;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		//check if i am dead
+		if (health <= 0){Destroy(this);}
+		
+		//update attack bools
+		coolCheck();
+		
+		//if an attack is ready launch it
+		if (attack1R) {attack1 ();}
+		if (attack2R) {attack2 ();}
+		if (attack3R) {attack3 ();}
+		
+		//somehow move on the path idk
 	}
 	#endregion
 }
--- toufuu_source/Assets/Scripts/Logic/Models/Path.cs
+++ toufuu_source/Assets/Scripts/Logic/Models/Path.cs
@@ -1,7 +1,7 @@
 using UnityEngine;
 using System.Collections;
 
-public class Path : MonoBehavior {
+public class Pather : MonoBehaviour {
 
 	//array of points make all z's zero
 	public Vector3[] points;
@@ -11,6 +11,13 @@ public class Path : MonoBehavior {
 	//counter amongst the array
 	public int pos;
 	
+	//function to adjust path to start position
+	public void pathInit(Vector3 me) {
+		for (int i = 0 ; i < points.length ; i++)
+		{points[i]=Vector3.add(points[i],me);}
+	}
+	
+	//takes the callers position and speed
 	public Vector3 getDir(Vector3 me, float speed) {
 		if (Vector3.Distance(me,points[pos]) < speed && pos!=points.length) {last=Vector3.Angle(me,points[pos]);}
 		return last;
