d1adb6106f9bb57ff0c628dd6e7c5b309384a13f
ArtificerZeno <picka@optonline.net>
2014-04-12 15:45:12 -0500

    made fowardshooter and made it so the first path of a point must be taken nomater what

---
+++ toufuu_source/Assets/Scripts/Logic/Models/FowardShooter.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FowardShooter : Enemy
+{
+    public void attack()
+    {
+        Transform nb = Object.Instantiate(E_Bullet, this.transform.position, Quaternion.identity) as Transform;
+        nb.gameObject.GetComponent<Pather>().points = mypath.points;
+        nb.gameObject.GetComponent<Pather>().pathInit(nb.position);
+    }
+
+    void Start()
+    {
+        myhealth = 5;
+        mycost = 10;
+        myspeed = 1;
+        mypath = this.GetComponent<Pather>();
+        attackC = 1;
+        attackL = Time.timeSinceLevelLoad;
+        attackR = true;
+    }
+}
